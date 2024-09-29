969e59417809126060b4196275dfe986cd9e70f2
Jacob Freeman <t00niyf5@duck.com>
2024-09-29 01:17:25 -0400

    Add disclaimer in code.

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
+/* # KlogClear
+ * Overwrites the kernel message buffer with crash dumps to hide previous contents
+ *
+ * # DISCLAIMER
+ * I wrote this for fun. It is not my responsibility what you do with it. Goodbye.
+ */
 
 #include <stdio.h>
 #include <unistd.h>
