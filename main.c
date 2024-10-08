809deb777545bec32c93981348d50629e894ac87
Jacob Freeman <t00niyf5@duck.com>
2024-10-08 02:30:05 -0400

    Improve logging.

--- main.c
+++ main.c
@@ -8,15 +8,18 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 524288 // A 512 KB Buffer
-#define CRASH_SIZE 89 // Worst case crash dump size
+#define BUFFER_SIZE (524288) // A 512 KB Buffer
+#define CRASH_SIZE (89) // Worst case crash dump size
 
 int main(void) {
     for (int so_far = 0;so_far < BUFFER_SIZE;so_far+=CRASH_SIZE) {
         if (fork()==0) {
+            // ReSharper disable once CppDFANullDereference
             return *(int*)NULL;
         }
-        printf("Filles up %d bytes so far\n",so_far);
+        printf("Filled up %i/%i bytes so far (%lf%%)\n",so_far, BUFFER_SIZE, (double)so_far/(double)BUFFER_SIZE*100);
     }
+    // Just to end it on 100.
+    printf("Filled up %i/%i bytes so far (100%%)\n",BUFFER_SIZE, BUFFER_SIZE);
     return 0;
 }
